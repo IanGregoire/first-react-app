68876072963707cf2e8de517fb91eb58c507e2a0
IanGregoire <ilghbr@gmail.com>
2021-11-14 11:10:50 -0500

    Filter buttons

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import Todo from "./components/Todo";
 import Form from "./Form";
 import FilterButton from "./FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.compelted,
+  Completed: task => task.compelted
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('all');
+
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
       if(id === task.id) {
@@ -15,12 +24,32 @@ function App(props) {
     });
     setTasks(updatedTasks);
   }  
-  const taskList = tasks.map(task => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} 
-    toggleTaskCompleted={toggleTaskCompleted} editTask={editTask} deleteTask={deleteTask}/>
-  ));
+  const taskList = tasks
+.filter(FILTER_MAP[filter])
+.map(task => (
+  <Todo
+    id={task.id}
+    name={task.name}
+    completed={task.completed}
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
+    editTask={editTask}
+  />
+));
+
   const tasksNoun = taskList.length != 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  
+
   function addTask(name) {
     const newTask = { id: "todo-"+nanoid() , name: name, compelted: false };
     setTasks([...tasks, newTask]);
@@ -38,14 +67,13 @@ function App(props) {
     const remainingTasks = tasks.filter(tasks => id !== task.id);
     setTasks(remainingTasks);
   }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
