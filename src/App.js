4ecd455a77ada91c6b77dfd1c4748dfaddf1d8e9
IanGregoire <ilghbr@gmail.com>
2021-11-14 10:34:50 -0500

    Use form to add tasks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./Form";
 import FilterButton from "./FilterButton";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks.map(task => (
     <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
   ));
+  const tasksNoun = taskList.length != 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  function addTask(name) {
+    const newTask = { id: "todo-"+nanoid() , name: name, compelted: false };
+    setTasks([...tasks, newTask]);
+  }
+  
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading"> 3 tasks remaining </h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
